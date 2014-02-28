936c23838e9727708a74ceef5becc7c06a429aed
Luis Lizarraga <siul.al4@gmail.com>
2014-02-27 19:36:58 -0600

    Ya se que hice

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -212,8 +212,8 @@ timer_interrupt (struct intr_frame *args UNUSED)
   struct dormido* d;
 
   
-    nod = list_begin(&dormidos),
-    d = list_entry(nod, struct dormido, nodo);
+    nodo = list_begin(&dormidos),
+    d = list_entry(nodo, struct dormido, nodo);
     if (d->por_dormir == ticks) {
       thread_unblock(d->t);
       list_remove(nodo);
